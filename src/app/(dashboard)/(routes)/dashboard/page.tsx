65df7f7f62a3b21b996a873822f632b93506c8ba
Joy Ahmed <dev.joyahmed@gmail.com>
2023-07-31 12:54:03 +0600

    organized route

---
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="container">
+      <h1 className="text-xl">Dashboard</h1>
+      <Button className="bg-teal-500 rounded-sm">Hello Btn</Button>
+    </div>
+  );
+};
+
+export default page;
